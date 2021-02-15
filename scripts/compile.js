7acd74e764434c7343a4eff5dde171f4188b09f4
Alexis Bronchart <alexis.bronchart@gmail.com>
2021-02-15 15:50:30 +0100

    feat(compile): optional source filename for compile command. Use --source-filename option (defaults to 'elium.po' for backward compatibility)

--- scripts/compile.js
+++ scripts/compile.js
@@ -9,21 +9,21 @@ const getDirectories = source =>
 		.map(name => join(source, name))
 		.filter(isDirectory)
 
-function compile(lang, poDir, jsonDir) {
-	gettextToI18next(lang, readFileSync(`${poDir}/${lang}/elium.po`)).then(
+function compile(lang, poDir, filename, jsonDir) {
+	gettextToI18next(lang, readFileSync(`${poDir}/${lang}/${filename}`)).then(
 		resources => writeFileSync(`${jsonDir}/${lang}.json`, resources),
 		err => console.error(err)
 	)
 }
 
-module.exports = async ({ source, target }) => {
+module.exports = async ({ source, sourceFilename, target }) => {
 	console.log('Compiling PO to JSON')
 	const directories = getDirectories(source, target)
 
 	directories.forEach(item => {
 		const locale = basename(item)
 		console.log(`Compiling ${locale}`)
-		compile(locale, source, target)
+		compile(locale, source, sourceFilename, target)
 	})
 	console.log('Finished')
 }
